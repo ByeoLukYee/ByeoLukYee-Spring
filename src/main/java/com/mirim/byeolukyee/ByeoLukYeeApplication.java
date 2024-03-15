838e964df4b6e4311d36d1fde200409fdcfa4a13
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-15 21:34:49 +0900

    first commit

---
+++ src/main/java/com/mirim/byeolukyee/ByeoLukYeeApplication.java
@@ -0,0 +1,13 @@
+package com.mirim.byeolukyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeoLukYeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByeoLukYeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mirim/byeolukyee/ByeoLukYeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirim.byeolukyee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByeoLukYeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
