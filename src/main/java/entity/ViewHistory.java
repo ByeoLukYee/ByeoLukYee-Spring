8a5abe08a13a56b17c97c0afeadf49d45026d1d9
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-16 12:54:11 +0900

    feat: Item 엔티티 추가

---
+++ src/main/java/entity/ViewHistory.java
@@ -0,0 +1,24 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ViewHistory extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private User user;
+
+    @ManyToOne(targetEntity = Item.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+}
+
