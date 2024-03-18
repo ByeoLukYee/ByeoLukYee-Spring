56158c03e9b2f6da6fbb35de36d81e06d110f378
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-18 19:47:55 +0900

    feat: SignInUserRequestDto 추가

---
+++ src/main/java/com/mirim/byeolukyee/dto/user/SignInUserRequestDto.java
@@ -0,0 +1,23 @@
+package com.mirim.byeolukyee.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignInUserRequestDto {
+    @Email
+    @NotBlank(message = "이메일은 공백일 수 없습니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{8,20}", message = "비밀번호는 8~20자 이내로 영문과 숫자로 이루어져야 합니다.")
+    private String password;
+}
