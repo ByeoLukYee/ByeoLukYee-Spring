11f20df048cb62cf0f640162371b18e145140327
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-18 18:52:49 +0900

    feat: 모든 user 조회, id로 user 조회, user 생성 엔드포인트 추가

---
+++ src/main/java/com/mirim/byeolukyee/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.mirim.byeolukyee.controller;
+
+import com.mirim.byeolukyee.dto.user.AddUserRequestDto;
+import com.mirim.byeolukyee.dto.user.UserResponseDto;
+import com.mirim.byeolukyee.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserResponseDto> getAllUsers() {
+        return userService.findAllUser();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto getUserById(@PathVariable Long id) {
+        return userService.findUserById(id);
+    }
+
+    @PostMapping
+    public UserResponseDto createUser(@RequestBody AddUserRequestDto addUserRequestDto) {
+        return userService.createUser(addUserRequestDto);
+    }
+}
