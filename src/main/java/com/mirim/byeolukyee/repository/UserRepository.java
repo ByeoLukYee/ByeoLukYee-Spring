2b94bc8ec7c8c31f3645d9db564bcf13539b9867
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-18 19:48:25 +0900

    feat: findByEmail 메서드 추가

--- src/main/java/com/mirim/byeolukyee/repository/UserRepository.java
+++ src/main/java/com/mirim/byeolukyee/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.mirim.byeolukyee.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
     Boolean existsUserByEmail(String email);
 }
