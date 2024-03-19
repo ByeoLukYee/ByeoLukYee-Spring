65e6dd19f41ad09368b6ebe716a7634f02b08f2e
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-19 19:39:45 +0900

    feat: 경매 참여 글 엔티티 추가

---
+++ src/main/java/com/mirim/byeolukyee/entity/BuyRequest.java
@@ -0,0 +1,23 @@
+package com.mirim.byeolukyee.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@DiscriminatorValue("BUY_REQUEST")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BuyRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "referenceItem")
+    private List<Item> referencingItems;
+}
