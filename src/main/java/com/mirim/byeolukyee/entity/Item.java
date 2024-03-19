fb3617cb85eea8fcef5bd482ea1c565a07651137
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-19 19:38:52 +0900

    feat: Item 엔티티를 부모 엔티티로 지정
    상속 전략 -> 싱글 테이블

--- src/main/java/com/mirim/byeolukyee/entity/Item.java
+++ src/main/java/com/mirim/byeolukyee/entity/Item.java
@@ -6,6 +6,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
 @Getter
 @Builder
 @AllArgsConstructor
@@ -22,20 +24,21 @@ public class Item extends BaseEntity {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
 
     @Column(nullable = false)
     private Integer price;
 
+    @Builder.Default
     @Column(nullable = false)
-    private String location = "미정";
+    private String location = "거래 희망 장소가 없습니다.";
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private ItemStatus status; // ON_SALE, RESERVED, SOLD_OUT
+    private ItemStatus status; // PROCEEDING, RESERVED, COMPLETE
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private ItemType type;  // BUYING, SELLING
+    private ItemType type;  // PRODUCT, BUY_REQUEST, SALE_OFFER
 }
