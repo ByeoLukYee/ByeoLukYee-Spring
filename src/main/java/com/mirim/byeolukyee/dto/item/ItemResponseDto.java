a4f32f5cd1450a1f9788d251c8560eecf5c86f39
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-20 22:52:44 +0900

    feat: ItemResponseDto 추가

---
+++ src/main/java/com/mirim/byeolukyee/dto/item/ItemResponseDto.java
@@ -0,0 +1,28 @@
+package com.mirim.byeolukyee.dto.item;
+
+import com.mirim.byeolukyee.dto.ResponseDto;
+import com.mirim.byeolukyee.dto.user.UserResponseDto;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ItemResponseDto extends ResponseDto {
+    private final Long id;
+    private final UserResponseDto user;
+    private final String title;
+    private final String description;
+    private final Integer price;
+    private final String location;
+
+
+    public ItemResponseDto(Boolean isDeleted, LocalDateTime createdAt, LocalDateTime updatedAt, Long id, UserResponseDto user, String title, String description, Integer price, String location) {
+        super(isDeleted, createdAt, updatedAt);
+        this.id = id;
+        this.user = user;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.location = location;
+    }
+}
