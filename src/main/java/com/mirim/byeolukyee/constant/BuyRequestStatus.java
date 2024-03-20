7241e5efd68d3460483653b42b9d83dfce2d1b49
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-20 22:22:06 +0900

    fix: Item 자식 엔티티의 각각의 Status 생성 및 한글 이름 추가

---
+++ src/main/java/com/mirim/byeolukyee/constant/BuyRequestStatus.java
@@ -0,0 +1,13 @@
+package com.mirim.byeolukyee.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BuyRequestStatus {
+    IN_PROGRESS("경매중"),
+    COMPLETE("경매완료");
+
+    private String krName;
+}
--- src/main/java/com/mirim/byeolukyee/constant/ItemStatus.java
+++
@@ -1,7 +0,0 @@
-package com.mirim.byeolukyee.constant;
-
-public enum ItemStatus {
-    PROCEEDING,
-    RESERVED,
-    COMPLETE
-}
