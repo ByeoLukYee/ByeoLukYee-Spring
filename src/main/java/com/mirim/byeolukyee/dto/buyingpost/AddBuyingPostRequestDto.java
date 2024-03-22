1c1c94e1b9a573bb3378e469040057d330dd5c5f
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-22 09:52:26 +0900

    feat: AddBuyingPostRequestDto 추가

---
+++ src/main/java/com/mirim/byeolukyee/dto/buyingpost/AddBuyingPostRequestDto.java
@@ -0,0 +1,9 @@
+package com.mirim.byeolukyee.dto.buyingpost;
+
+import com.mirim.byeolukyee.dto.post.AddPostRequestDto;
+import lombok.Getter;
+
+@Getter
+public class AddBuyingPostRequestDto extends AddPostRequestDto  {
+
+}
