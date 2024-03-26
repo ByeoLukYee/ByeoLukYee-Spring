e448acbe7e598063723a8d9f089de93796eeec26
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-26 16:48:00 +0900

    fix: SellingPostStatue -> BuyingPostStatus

--- src/main/java/com/mirim/byeolukyee/dto/buyingpost/UpdateBuyingPostRequestDto.java
+++ src/main/java/com/mirim/byeolukyee/dto/buyingpost/UpdateBuyingPostRequestDto.java
@@ -1,10 +1,10 @@
 package com.mirim.byeolukyee.dto.buyingpost;
 
-import com.mirim.byeolukyee.constant.SellingPostStatus;
+import com.mirim.byeolukyee.constant.BuyingPostStatus;
 import com.mirim.byeolukyee.dto.post.UpdatePostRequesetDto;
 import lombok.Getter;
 
 @Getter
 public class UpdateBuyingPostRequestDto extends UpdatePostRequesetDto {
-    private SellingPostStatus status;
+    private BuyingPostStatus status;
 }
