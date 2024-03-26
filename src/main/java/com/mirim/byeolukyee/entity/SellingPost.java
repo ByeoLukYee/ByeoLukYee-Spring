03be31689583ca8438211c61ce2d31ab86831533
hyeg0121 <w2218@e-mirim.hs.kr>
2024-03-26 16:09:09 +0900

    coment: Todo 제거

--- src/main/java/com/mirim/byeolukyee/entity/SellingPost.java
+++ src/main/java/com/mirim/byeolukyee/entity/SellingPost.java
@@ -17,7 +17,4 @@ public class SellingPost extends Post {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private SellingPostStatus status = SellingPostStatus.IN_PROGRESS;
-
-    // TODO: 좋아요 수 추가
-    // TODO: 조회수 추가
 }
